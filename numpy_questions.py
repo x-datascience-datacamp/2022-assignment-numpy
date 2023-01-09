e404d9695280db94336fa0c82dd00bfa8a2febd0
ahmedmernissi20 <96150401+ahmedmernissi20@users.noreply.github.com>
2023-01-09 16:12:15 +0100

    first commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -37,9 +37,7 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
-
     # TODO
-
     if not isinstance(X, np.ndarray):
         raise ValueError("X must be a numpy array")
     if X.ndim != 2:
