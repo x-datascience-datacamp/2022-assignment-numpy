60706e6cf633b86c24d434decf203592960aec8b
eva-clrg <e32.clergue@gmail.com>
2023-01-09 16:22:24 +0100

    modif for flake8

--- numpy_questions.py
+++ numpy_questions.py
@@ -72,6 +72,6 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
     pi = 1
-    for i in range(1,n_terms+1):
+    for i in range(1, n_terms+1):
         pi = pi * (4*i**2) / (4*i**2-1)
     return pi*2
