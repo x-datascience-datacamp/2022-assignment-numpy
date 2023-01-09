7d5f24164081a0612f75e1882cb10f161df9e42f
AMINE EL KARI <74050425+Amine-elk@users.noreply.github.com>
2023-01-09 04:15:17 -1100

    new test

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,20 +1,4 @@
-"""Assignment - using numpy and making a PR.
 
-The goals of this assignment are:
-    * Use numpy in practice with two easy exercises.
-    * Use automated tools to validate the code (`pytest` and `flake8`)
-    * Submit a Pull-Request on github to practice `git`.
-
-The two functions below are skeleton functions. The docstrings explain what
-are the inputs, the outputs and the expected error. Fill the function to
-complete the assignment. The code should be able to pass the test that we
-wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
-the repo. It should say that 2 tests ran with success.
-
-We also ask to respect the pep8 convention: https://pep8.org.
-This will be enforced with `flake8`. You can check that there is no flake8
-errors by calling `flake8` at the root of the repo.
-"""
 import numpy as np
 
 
@@ -39,21 +23,18 @@ def max_index(X):
     """
     i = 0
     j = 0
-    
-    # TODO
 
+    # TODO
     if not isinstance(X, np.ndarray):
         raise ValueError("X must be a numpy array")
     if X.ndim != 2:
         raise ValueError("X must be a 2D array")
 
-    i, j = np.unravel_index(np.argmax(X), X.shape)
-    
-    return i, j 
+    max_val = np.max(X)
+    k, h = np.where(X == max_val)
+    i, j = k[0], h[0]
 
-
-
-
+    return i, j
 
 
 def wallis_product(n_terms):
@@ -73,6 +54,8 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
+    # XXX : The n_terms is an int that corresponds to the number of
+    # terms in the product. For example 10000.
     pi = 1
     for i in range(1, n_terms+1):
         pi *= (2*i) / (2*i - 1) * (2*i) / (2*i + 1)
