19ad861ce1b60eb24f271b104b1dc7dc7b75ae18
Abdelghani4 <harradabdelghani@gmail.com>
2023-01-09 15:45:14 +0100

    third commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -72,4 +72,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 1
+    for i in range(1, n_terms+1):
+        pi *= (2*i) / (2*i - 1) * (2*i) / (2*i + 1)
+    return pi * 2
