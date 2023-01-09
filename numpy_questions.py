9c6428d1ff6bb3da008bb33e1a3fa61c335cd591
thibautvalour <74783392+thibautvalour@users.noreply.github.com>
2023-01-09 22:25:55 +0100

    erase white space in blank line

--- numpy_questions.py
+++ numpy_questions.py
@@ -19,8 +19,8 @@ import numpy as np
 
 
 def max_index(X):
-
     """Return the index of the maximum in a numpy array.
+
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
@@ -47,8 +47,8 @@ def max_index(X):
 
 
 def wallis_product(n_terms):
-
     """Implement the Wallis product to compute an approximation of pi.
+
     See:
     https://en.wikipedia.org/wiki/Wallis_product
     Parameters
