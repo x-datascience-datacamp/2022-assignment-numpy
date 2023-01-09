1dcab9b41113c3e967b18fb6b5744d3aa4b1d731
taher-asmi <asmi.taher86@gmail.com>
2023-01-09 15:54:13 +0100

    Mriguel

--- numpy_questions.py
+++ numpy_questions.py
@@ -66,4 +66,8 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    wallis = 1
+    if  n_terms!=0:
+        for j in range(n_terms):
+            wallis = wallis * (2*j/(2*j-1))*(2*j/(2*j+1))
+    return wallis 
