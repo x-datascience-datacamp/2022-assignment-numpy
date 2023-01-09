c09b2b630a89a20a3ab69a02c75c07b888b01500
Windu12345 <32morvantheo@gmail.com>
2023-01-09 14:19:08 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -38,18 +38,18 @@ def max_index(X):
         if the shape is not 2D.
     """
     try:
-        type(X)==np.ndarray
-    except :
-        raise ValueError('The input must be a numpy array')
-    
+        type(X) == np.ndarray
+    except:
+        raise ValueError("The input must be a numpy array")
+
     try:
-        X.ndim==2
-    except :
-        raise ValueError('The input must be a 2D np.array')
+        X.ndim == 2
+    except:
+        raise ValueError("The input must be a 2D np.array")
     i = 0
     j = 0
 
-    i,j = np.unravel_index(X.argmax(), X.shape)
+    i, j = np.unravel_index(X.argmax(), X.shape)
     return i, j
 
 
@@ -72,14 +72,15 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    if n_terms==0:
+    if n_terms == 0:
         return 2
+
     # numbers_to_be_multiplied = np.arange(1, n_terms+1)
     # values = 4*numbers_to_be_multiplied**2/(4*numbers_to_be_multiplied**2-1)
     # pi_estimated = values.cumprod()[-1]
+
     pi_estimated = 1
-    for j in range(1,n_terms+1):
-        pi_estimated*= 4*j**2/(4*j**2-1)
-    # print(pi_estimated)
-    pi_estimated *=2
+    for j in range(1, n_terms + 1):
+        pi_estimated *= 4 * j**2 / (4 * j**2 - 1)
+    pi_estimated *= 2
     return pi_estimated
