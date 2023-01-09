34a70ede2dcec97e2aa7dd264dd67c7e86478868
尚宗 <shangzong@shangzongdeMacBook-Pro.local>
2023-01-09 15:35:39 +0100

    first commit

--- numpy_questions.py
+++ numpy_questions.py
@@ -39,8 +39,12 @@ def max_index(X):
     """
     i = 0
     j = 0
+    if type(X) !=np.ndarray:
+        raise ValueError()
+    if X.ndim != 2:
+        raise ValueError()
 
-    # TODO
+    [i,j] = np.where(X==np.max(X))
 
     return i, j
 
@@ -64,4 +68,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    pi = 1
+    if n_terms == 0:
+        return 2
+    for n in range(1,n_terms+1):
+        pi = pi * 4*n*n/(4*n*n-1)
+    pi = pi*2
+
+    return pi
