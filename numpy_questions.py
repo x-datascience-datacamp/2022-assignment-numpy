d8e5684e625c2ca4774d0bf5cc264abbd258db29
Romain Louvet <romain.louvet@telecom-paris.fr>
2023-01-09 15:14:35 +0100

    Second function

--- numpy_questions.py
+++ numpy_questions.py
@@ -47,7 +47,7 @@ def max_index(X):
     if len(X.shape)!=2 : 
         raise ValueError
     i,j = np.where(X == np.max(X))
-    
+
     return i, j
 
 
@@ -70,4 +70,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    p = 2*1.
+    if n_terms == 0 : 
+        return p 
+
+    for i in range(1,n_terms+1) :
+        p*= (2*i)**2/((2*i-1)*(2*i+1))
+
+    return p
