9947baf8d0238cbdeebf709ed70975526e187f19
Miralles <miralles.gauthier@gmail.com>
2023-01-09 17:11:02 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -38,9 +38,9 @@ def max_index(X):
         if the shape is not 2D.
     """
     if type(X) is not np.ndarray:
-        raise( ValueError('The input is not a numpy array') )
+        raise ValueError('The input is not a numpy array') 
     if len(X.shape) != 2:
-        raise(ValueError('The shape of the input is not 2D'))
+        raise ValueError('The shape of the input is not 2D')
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
     return(i, j)
 
