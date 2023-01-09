e46b41004a52562f532d07b652bde6d417568a9a
Fatima-Ezzahra Fettah <fatima-ezzahra.fettah@emines.um6p.ma>
2023-01-09 14:42:03 +0100

    myfirstcommit

--- numpy_questions.py
+++ numpy_questions.py
@@ -41,9 +41,14 @@ def max_index(X):
     j = 0
 
     # TODO
+    if not isinstance(X,np.ndarray) or len(X.shape) !=2 :
+        raise ValueError('non relevant format')
+    else:
+        i, j = np.unravel_index(X.argmax(), X.shape)
+        return i, j
+    #return i, j
 
-    return i, j
-
+import functools
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -64,4 +69,14 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2
+    else:
+        output = 0
+        for i in range (1, n_terms):
+            print(i)
+            output += (4.0*(i**2))/(4.0*(i**2)-1) 
+        return 2*output
+
+
+
