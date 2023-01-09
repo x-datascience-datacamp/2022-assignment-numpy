833b533573bb32e583d70fab04481f6de8754407
Ahmad-Khalifeh-11 <khalifeh-ahmad@hotmail.com>
2023-01-09 22:44:37 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -59,7 +59,7 @@ def max_index(X):
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
 
-    See: 
+    See:
     https://en.wikipedia.org/wiki/Wallis_product
 
     Parameters
