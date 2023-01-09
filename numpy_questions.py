86b43d538d0eba31bb2deec1d568458107dbc960
Mathys Noir <mathys.noir@edu.ece.fr>
2023-01-09 15:04:34 +0100

    UP without loop

--- numpy_questions.py
+++ numpy_questions.py
@@ -67,8 +67,6 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    pi = 1
-    for i in range(1, n_terms + 1):
-        pi *= (4 * i ** 2) / (4 * i ** 2 - 1)
-    pi *= 2
-    return pi
+    pi = np.arange(1, n_terms + 1)
+    pi = np.prod(2*pi/(2*pi-1))*np.prod(2*pi/(2*pi+1))
+    return pi * 2
