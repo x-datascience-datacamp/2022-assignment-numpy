2f5447d432a3c71d9d4fa6448dfe5cfed3b95140
Yassine Hargane <yassine.hargane@telecom-paris.fr>
2023-01-09 15:45:54 +0100

    2e question

--- numpy_questions.py
+++ numpy_questions.py
@@ -69,4 +69,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+
+    product = 1
+    for i in range(1, n_terms + 1):
+        term = (2 * i) / (2 * i - 1) * (2 * i) / (2 * i + 1)
+        product *= term
+    return product * 2
+
+
