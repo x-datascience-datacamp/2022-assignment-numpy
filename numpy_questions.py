fd7f158a738582f22e5593238d9f7300af57fda4
Sajjad Mahdavi <sajjad.mahdavi@universite-paris-saclay.fr>
2023-01-09 19:17:53 +0100

    V1_numpy

--- numpy_questions.py
+++ numpy_questions.py
@@ -67,7 +67,8 @@ def wallis_product(n_terms):
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
     s = 1
-    for i in range(1,n_terms+1):
-        s*=(4*i**2)/(4*i**2-1)
+    for i in range(1, n_terms+1):
+        s = s * (4 * i**2) /( 4 * i**2 - 1)
 
     return 2*s
+    
