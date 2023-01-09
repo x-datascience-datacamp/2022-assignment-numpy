0fe8fbe49ce6944958493d13477011056521e78d
najid110 <najid110@gmail.com>
2023-01-09 19:51:30 +0100

    Numpy assignment najid110 better

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,9 +1,25 @@
+"""Assignment - using numpy and making a PR.
+
+The goals of this assignment are:
+    * Use numpy in practice with two easy exercises.
+    * Use automated tools to validate the code (`pytest` and `flake8`)
+    * Submit a Pull-Request on github to practice `git`.
+
+The two functions below are skeleton functions. The docstrings explain what
+are the inputs, the outputs and the expected error. Fill the function to
+complete the assignment. The code should be able to pass the test that we
+wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
+the repo. It should say that 2 tests ran with success.
+
+We also ask to respect the pep8 convention: https://pep8.org.
+This will be enforced with `flake8`. You can check that there is no flake8
+errors by calling `flake8` at the root of the repo.
+"""
 import numpy as np
 
 
 def max_index(X):
-    """
-    Return the index of the maximum in a numpy array.
+    """Return the index of the maximum in a numpy array.
 
     Parameters
     ----------
@@ -13,7 +29,7 @@ def max_index(X):
     Returns
     -------
     (i, j) : tuple(int)
-        The row and column index of the maximum.
+        The row and columnd index of the maximum.
 
     Raises
     ------
@@ -21,25 +37,23 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+    i = 0
+    j = 0
+
     if not isinstance(X, np.ndarray):
-        raise ValueError("X must be a numpy array.")
-    if len(X.shape) != 2:
-        raise ValueError("X must be 2D.")
+        raise ValueError("X must be a numpy array")
+    if X.ndim != 2:
+        raise ValueError("X must be a 2D array")
 
-    max_val = X[0][0]
-    max_ind = (0, 0)
-    for i in range(X.shape[0]):
-        for j in range(X.shape[1]):
-            if X[i][j] > max_val:
-                max_val = X[i][j]
-                max_ind = (i, j)
+    max_val = np.max(X)
+    k, h = np.where(X == max_val)
+    i, j = k[0], h[0]
 
-    return max_ind
+    return i, j
 
 
 def wallis_product(n_terms):
-    """
-    Implement the Wallis product to compute an approximation of pi.
+    """Implement the Wallis product to compute an approximation of pi.
 
     See:
     https://en.wikipedia.org/wiki/Wallis_product
@@ -55,13 +69,9 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    if n_terms < 0:
-        raise ValueError("n_terms must be a non-negative integer.")
-    elif n_terms == 0:
-        return 2.
-
-    pi = 1.
+    # XXX : The n_terms is an int that corresponds to the number of
+    # terms in the product. For example 10000.
+    pi = 1
     for i in range(1, n_terms+1):
-        pi *= ((2*i)**2) / ((2*i-1)*(2*i+1))
-
-    return 2 * pi
+        pi *= (2*i) / (2*i - 1) * (2*i) / (2*i + 1)
+    return pi * 2
