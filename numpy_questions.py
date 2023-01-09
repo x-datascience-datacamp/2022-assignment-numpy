fcc8af2ce49c8c6d0f18ad98bc448b9d61a7478f
zack242 <zakaria_009@hotmail.fr>
2023-01-09 15:07:37 +0100

    Add functions max_index and wallis_product

--- numpy_questions.py
+++ numpy_questions.py
@@ -39,11 +39,15 @@ def max_index(X):
     """
     i = 0
     j = 0
+    
+    if not isinstance(X, np.ndarray):
+        raise ValueError("The input is not a numpy array")  
+    if len(X.shape) != 2:
+        raise ValueError("The shape is not 2D")
+    
+    i,j=np.where(X == np.max(X))
 
-    # TODO
-
-    return i, j
-
+    return i,j
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -62,6 +66,9 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    #using numpy 
+    pi = 1
+    for i in range(1, n_terms+1):
+        pi *= 4*(i**2)/(4*(i**2)-1)
+    pi *= 2
+    return pi
