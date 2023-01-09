facde9471e67bbaef9a959cc0faa36504114db28
Ihssane Ghalas <ihssaneghalas123@gmail.com>
2023-01-09 15:16:26 +0100

    First assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -40,6 +40,10 @@ def max_index(X):
     i = 0
     j = 0
     # TODO
+    if type(X) != np.ndarray:
+        raise(ValueError)
+    if len(X.shape) != 2: 
+        raise(ValueError)
     val = X[0][0]
     for s in range(len(X)):
         for l in range(len(X[0])): 
@@ -70,4 +74,10 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    res = 2
+    if n_terms == 0 :
+        return res
+
+    for n in range(1, n_terms+1) :
+        res *= (4*(n**2))/(4*(n**2) - 1)
+    return res
