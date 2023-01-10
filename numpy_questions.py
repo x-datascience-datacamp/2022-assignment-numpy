7f04d5420380c15ad464622891eb19c33d0a4bf2
Yang ZHANG <92279546+yangzhang33@users.noreply.github.com>
2023-01-10 01:12:53 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -41,8 +41,12 @@ def max_index(X):
     j = 0
 
     # TODO
-    i = 1
-    i = i
+    if (type(X) is np.ndarray) and (X.ndim == 2):
+        i, j = np.where(X == np.max(X))
+        i, j = i[0], j[0]
+    else:
+        raise ValueError
+
     return i, j
 
 
@@ -65,4 +69,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if not isinstance(n_terms, int) or n_terms < 0:
+        raise ValueError
+    if n_terms == 0:
+        return 2.
+    pi = 1.
+    for i in range(1, n_terms+1):
+        pi *= (4*i**2) / (4*i**2-1)
+    return 2*pi
