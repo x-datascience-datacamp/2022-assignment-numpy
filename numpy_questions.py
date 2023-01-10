6710dad672ad2142b2054b54da70b945770f78e6
aimiokab <62746507+aimiokab@users.noreply.github.com>
2023-01-10 14:35:30 +0100

    Update numpy_questions.py

--- numpy_questions.py
+++ numpy_questions.py
@@ -49,7 +49,7 @@ def max_index(X):
             for y in range(X.shape[1]):
                 if X[x][y] > max:
                     max = X[x][y]
-                    i, j = x, y 
+                    i, j = x, y
         return i, j
 
 
@@ -76,4 +76,4 @@ def wallis_product(n_terms):
     if n_terms > 0:
         for k in range(1, n_terms+1):
             wallis = wallis*(4*k**2/(4*k**2-1))
-    return wallis 
+    return wallis
