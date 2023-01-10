cc52a4a09a41b3c47129f11df0a6a3c1012201ac
Dave Dahan <davedahan@MacBook-Pro-de-Dave.local>
2023-01-10 16:18:22 +0100

    fix

--- numpy_questions.py
+++ numpy_questions.py
@@ -68,7 +68,7 @@ def wallis_product(n_terms):
 
     Returns
     -------
-    pi : float 
+    pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
     # XXX : The n_terms is an int that corresponds to the number of
