b3c494256e48cb1d144d060db72b216148206253
MohamedelfakirX <mohamed.elfakir.pro@gmail.com>
2023-01-11 02:32:32 +0100

    Complete Code

--- numpy_questions.py
+++ numpy_questions.py
@@ -21,14 +21,17 @@ import numpy as np
 
 def max_index(X):
     """Return the index of the maximum in a numpy array.
+
     Parameters
     ----------
     X : ndarray of shape (n_samples, n_features)
         The input array.
+
     Returns
     -------
     (i, j) : tuple(int)
         The row and columnd index of the maximum.
+
     Raises
     ------
     ValueError
@@ -40,35 +43,37 @@ def max_index(X):
 
     # TODO
     if not isinstance(X, np.ndarray):
-        raise ValueError("input an array")
+        raise ValueError("X must be a numpy array")
     if X.ndim != 2:
-        raise ValueError("input a 2D array")
-    i, j = np.unravel_index(X.argmax(), X.shape)
+        raise ValueError("X must be a 2D array")
+
+    max_val = np.max(X)
+    k, h = np.where(X == max_val)
+    i, j = k[0], h[0]
 
     return i, j
 
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
+
     See:
     https://en.wikipedia.org/wiki/Wallis_product
+
     Parameters
     ----------
     n_terms : int
-        Number of steps in the Wallis product. Note that n_terms=0 will
-        consider the product to be 1.
+        Number of steps in the Wallis product. Note that `n_terms=0` will
+        consider the product to be `1`.
+
     Returns
     -------
     pi : float
-        The approximation of order n_terms of pi using the Wallis product.
+        The approximation of order `n_terms` of pi using the Wallis product.
     """
     # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000
-
-    product = 1
-
-    for i in range(1, n_terms + 1):
-        term = (2 * i) / (2 * i - 1) * (2 * i) / (2 * i + 1)
-        product *= term
-
-    return 2 * product
+    # terms in the product. For example 10000.
+    pi = 1
+    for i in range(1, n_terms+1):
+        pi *= (2*i) / (2*i - 1) * (2*i) / (2*i + 1)
+    return pi * 2
