8c6d9e3ec6c14368931a4aeef64fbcdd5eac69fb
sonalimp29 <mp.sonali29@gmail.com>
2023-01-12 15:01:50 +0100

    first_test2

--- numpy_questions.py
+++ numpy_questions.py
@@ -82,6 +82,7 @@ def wallis_product(n_terms):
         raise ValueError
     if n_terms == 0:
         return 2.
+
     pi = 1.
     for i in range(1, n_terms+1):
         pi *= (4*i**2) / (4*i**2-1)
