beb93ffbc4abd3d1b962ac10fe3f8a87fb83bd9f
Angie Méndez <almendezl@unal.edu.co>
2023-01-12 21:17:32 +0100

    Solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -45,7 +45,6 @@ def max_index(X):
         raise ValueError("The input must be a 2D numpy array")
         
     i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
-    
     return i, j
 
 
@@ -68,9 +67,8 @@ def wallis_product(n_terms):
     """
     pi = 1.
     # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    
+    # terms in the product. For example 10000.    
     for i in range(1, n_terms+1):
-        pi *= 4*(i**2)/(4*(i**2)-1) 
+        pi *= 4*(i**2)/(4*(i**2)-1)
 
     return pi * 2
