171d710dbdca1d905b5a60bf34643c67396983cc
rboggio <richard.boggio@polytechnique.edu>
2023-01-13 09:35:45 +0100

    Final

--- numpy_questions.py
+++ numpy_questions.py
@@ -67,12 +67,12 @@ def wallis_product(n_terms):
     -------
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
-    """        
+    """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
     pi = 1
 
-    if n_terms==0:
+    if n_terms == 0:
         return 2
 
     for i in range(1, n_terms+1):
